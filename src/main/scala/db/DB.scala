84642bee7f984605bdd1521150d62073df140ed7
wolfgang <wolfgang@localhost>
2015-04-07 22:52:39 +0200

    database upgrade works perfectly!

--- src/main/scala/db/DB.scala
+++ src/main/scala/db/DB.scala
@@ -1,39 +1,32 @@
 package db
 
 import java.io.File
-import java.sql.{SQLNonTransientConnectionException, SQLException}
+
 import org.squeryl.adapters.DerbyAdapter
 import org.squeryl.PrimitiveTypeMode._
 import org.squeryl.dsl._
 import org.squeryl._
-import util._
+
 import org.squeryl.annotations.Transient
-import framework.Logging
-import FileHelper._
+
+import util._
 import util.AppStorage
+import framework.Logging
+
+import scala.language.postfixOps
 
 /*
 
   KEEP it simple! don't add unneeded stuff (keys, indices, constraints)...
+  * keep the primary keys, don't add 'identity' columns with auto-increment. Better for DB manipulation!
 
-  after migration etc, check:
-    * squirrelsql, table: rightclick->scripts->show table script MUST exactly be the same for old and new db!
-  TODO:
-    * more indices on columns?
-    * identity columns with auto-increment?
-       if i want an identity column with auto-index stuff... did not exist as reftool4 was done.
-       the new schema def makes an identity column with auto-incrementing ID...
-       this is not in reftool4 db, and adding it doesn't work yet (10.12): https://issues.apache.org/jira/browse/DERBY-3888
-       http://stackoverflow.com/questions/7295391/how-to-alter-column-from-primary-key-to-identity-for-derby
-          however, inserting into an identity-column is not possible or very hard.... grrrrr
-       so, for now, I don't use "is autoIncrement" above and do auto-increment by hand in callbacks!
+  after migration etc, check using dblook that upgraded database ddl matches generated one!
 
  */
 
 
 class BaseEntity extends KeyedEntity[Long] {
   var id: Long = 0
-  //  var lastModified = new TimestampType(System.currentTimeMillis) // TODO: add?
 }
 
 class Setting(var name: String = "", var value: String = "") extends BaseEntity
@@ -113,7 +106,7 @@ object ReftoolDB extends Schema with Logging {
     via[Topic2Article]((t, a, ta) => (ta.TOPIC === t.id, a.id === ta.ARTICLE))
 
   on(topics2articles)(a => declare(
-    a.color is named("COLOR"), a.color defaultsTo(0)
+    a.color is named("COLOR"), a.color defaultsTo 0
   ))
 
   // manually auto-increment ids.
@@ -122,128 +115,17 @@ object ReftoolDB extends Schema with Logging {
     beforeInsert(topics) call((x:Topic) => x.id = from(topics)(a => select(a).orderBy(a.id desc)).headOption.getOrElse(new Topic()).id + 1)
   )
 
-  // upgrades old reftool4 database
-  // what a mess, but this is due to hibernate <> squeryl... and now I understand everything!
-  def upgrade4to5() {
-    info("Upgrade4to5 of " + AppStorage.config.olddbpath + " ...")
-
-    def dbShutdown(dbpath: String): Unit = {
-      try { java.sql.DriverManager.getConnection(s"jdbc:derby:$dbpath;shutdown=true") } catch {
-        case se: SQLException => if (!se.getSQLState.equals("08006")) throw se
-        case se: SQLNonTransientConnectionException => if (!se.getSQLState.equals("08006")) throw se
-      }
-    }
-
-    def dbStats(dbpath: String, clobx: Boolean): Unit = {
-      val dbconnx = java.sql.DriverManager.getConnection(s"jdbc:derby:$dbpath")
-      val s = dbconnx.createStatement()
-      var r = s.executeQuery("select COUNT(*) as rowcount from ARTICLES")
-      r.next()
-      info("  article count=" + r.getInt("rowcount"))
-      r = s.executeQuery("select COUNT(*) as rowcount from TOPICS")
-      r.next()
-      info("  topics count=" + r.getInt("rowcount"))
-      def printClobSize(table: String, col: String, clob: Boolean): Unit = {
-        r = s.executeQuery(s"select $col from $table")
-        var sl: Long = 0
-        while (r.next()) {
-          if (clob) {
-            val cl = r.getClob(col)
-            if (cl != null) sl += cl.length()
-          } else {
-            val ss = r.getString(col)
-            if (ss != null) sl += ss.length
-          }
-        }
-        info(s"  total $col character count = " + sl)
-      }
-      printClobSize("ARTICLES", "REVIEW", clobx)
-      printClobSize("ARTICLES", "BIBTEXENTRY", clobx)
-      printClobSize("ARTICLES", "PDFLINK", clob = false)
-      dbconnx.close()
-      dbShutdown(dbpath)
-    }
-    // clean
-    val pdir = new File(AppStorage.config.dbpath)
-    FileHelper.deleteAll(pdir) // TODO remove later...
-    //    val w = new java.io.PrintWriter(new java.io.OutputStreamWriter(System.out))
-    //    java.sql.DriverManager.setLogWriter(w)
-    debug("checking old db...")
-    dbStats(AppStorage.config.olddbpath, clobx = true)
-    debug("importing db...")
-    java.sql.DriverManager.getConnection(s"jdbc:derby:${AppStorage.config.dbpath};createFrom=${AppStorage.config.olddbpath}")
-    dbShutdown(AppStorage.config.dbpath)
-    debug("upgrading db...")
-    java.sql.DriverManager.getConnection(s"jdbc:derby:${AppStorage.config.dbpath};upgrade=true")
-    dbShutdown(AppStorage.config.dbpath)
-    debug("modify db...")
-    val dbc = java.sql.DriverManager.getConnection(s"jdbc:derby:${AppStorage.config.dbpath}")
-    val s = dbc.createStatement()
-    def modCol(table: String, col: String, default: String = null) = {
-      if (default != null) {
-        s.execute(s"update $table set $col=$default WHERE $col IS NULL")
-        s.execute(s"alter table $table alter column $col default $default")
-      }
-      s.execute(s"alter table $table alter column $col not null")
-    }
-    for (c <- List("TAGS", "KEYWORDS", "RESEARCHGROUP", "CITING", "CITED", "RATING", "PARENT", "PARENT_ID", "AID"))
-      s.execute("alter table ARTICLES drop column " + c)
-
-    s.execute("drop table ARTICLE2ARTICLE")
-
-    modCol("SETTING", "NAME", "''")
-    modCol("SETTING", "VALUE", "''")
-
-    s.execute("alter table TOPIC2ARTICLE drop column PREDECESSORINTOPIC")
-    s.execute("alter table TOPIC2ARTICLE drop column ID") // not needed!
-    modCol("TOPIC2ARTICLE", "TOPIC")
-    modCol("TOPIC2ARTICLE", "ARTICLE")
-    modCol("TOPIC2ARTICLE", "COLOR", "0")
-    s.execute("alter table TOPIC2ARTICLE drop foreign key FK345DBEB334742380")
-    s.execute("alter table TOPIC2ARTICLE drop foreign key FK345DBEB3187A628E")
-    s.execute("""ALTER TABLE "APP"."TOPIC2ARTICLE" ADD CONSTRAINT "TOPIC2ARTICLECPK" UNIQUE ("TOPIC", "ARTICLE")""") // from ddl comparison
-    //TODO: this does not work. check what it's about, probably data inconsistent!
-    s.execute("alter table TOPICS drop foreign key FKCC42D924F2885EFB")
-    s.execute("alter table TOPICS drop foreign key FKCC42D924CF8E6BFD")
-    s.execute("update TOPICS SET PARENT=0 WHERE PARENT IS NULL")
-    modCol("TOPICS", "PARENT")
-    s.execute("alter table TOPICS add column EXPANDED boolean not null default false")
-    s.execute("alter table TOPICS alter column TITLE set data type VARCHAR(512)")
-    modCol("TOPICS", "TITLE", "''")
-
-    s.execute("alter table ARTICLES alter column AUTHORS set data type VARCHAR(4096)")
-    s.execute("alter table ARTICLES alter column PDFLINK set data type VARCHAR(10000)")
-    s.execute("alter table ARTICLES drop foreign key FKB6C0D23D4421A2B3")
-
-    s.execute("alter table ARTICLES add column REVIEWNEW VARCHAR(10000)")
-    s.execute("update ARTICLES set REVIEWNEW=REVIEW")
-    s.execute("alter table ARTICLES drop column REVIEW")
-    s.execute("rename column ARTICLES.REVIEWNEW to REVIEW")
-
-    s.execute("alter table ARTICLES add column BIBTEXENTRYNEW VARCHAR(10000)")
-    s.execute("update ARTICLES set BIBTEXENTRYNEW=BIBTEXENTRY")
-    s.execute("alter table ARTICLES drop column BIBTEXENTRY")
-    s.execute("rename column ARTICLES.BIBTEXENTRYNEW to BIBTEXENTRY")
-
-    for (cc <- List("BIBTEXENTRY", "REVIEW", "JOURNAL", "BIBTEXID", "LINKURL", "AUTHORS", "ENTRYTYPE", "PDFLINK", "TITLE", "DOI", "PUBDATE"))
-      modCol("ARTICLES", cc, "''")
-
-    dbc.close()
-    dbShutdown(AppStorage.config.dbpath)
-    debug("checking new db...")
-    dbStats(AppStorage.config.dbpath, clobx = false)
-    info("Upgrade4to5 finished!")
-  }
 
   def initialize() {
 
-    val startwithempty = false
+
+    val startwithempty = true // true: create schema with squeryl ; false: migrate old db
 
     // TODO DB selection dialog etc
     if (!startwithempty) {
       if (!new java.io.File(AppStorage.config.dbpath).exists()) {
-        upgrade4to5()
-        new File(AppStorage.config.pdfpath).mkdir() // TODO
+        DBupgrades.upgrade4to5()
+        new File(AppStorage.config.pdfpath).mkdir()
       }
     } else {
       FileHelper.deleteAll(new java.io.File(AppStorage.config.dbpath))
---
+++ src/main/scala/db/DBupgrades.scala
@@ -0,0 +1,126 @@
+package db
+
+import java.io.File
+import java.sql.{SQLNonTransientConnectionException, SQLException}
+
+import framework.Logging
+import util.{FileHelper, AppStorage}
+
+
+object DBupgrades extends Logging {
+
+  // upgrades old reftool4 database
+  // what a mess, but this is due to hibernate <> squeryl... and now I understand everything!
+  def upgrade4to5() {
+    info("Upgrade4to5 of " + AppStorage.config.olddbpath + " ...")
+
+    def dbShutdown(dbpath: String): Unit = {
+      try { java.sql.DriverManager.getConnection(s"jdbc:derby:$dbpath;shutdown=true") } catch {
+        case se: SQLException => if (!se.getSQLState.equals("08006")) throw se
+        case se: SQLNonTransientConnectionException => if (!se.getSQLState.equals("08006")) throw se
+      }
+    }
+
+    def dbStats(dbpath: String, clobx: Boolean): Unit = {
+      val dbconnx = java.sql.DriverManager.getConnection(s"jdbc:derby:$dbpath")
+      val s = dbconnx.createStatement()
+      var r = s.executeQuery("select COUNT(*) as rowcount from ARTICLES")
+      r.next()
+      info("  article count=" + r.getInt("rowcount"))
+      r = s.executeQuery("select COUNT(*) as rowcount from TOPICS")
+      r.next()
+      info("  topics count=" + r.getInt("rowcount"))
+      def printClobSize(table: String, col: String, clob: Boolean): Unit = {
+        r = s.executeQuery(s"select $col from $table")
+        var sl: Long = 0
+        while (r.next()) {
+          if (clob) {
+            val cl = r.getClob(col)
+            if (cl != null) sl += cl.length()
+          } else {
+            val ss = r.getString(col)
+            if (ss != null) sl += ss.length
+          }
+        }
+        info(s"  total $col character count = " + sl)
+      }
+      printClobSize("ARTICLES", "REVIEW", clobx)
+      printClobSize("ARTICLES", "BIBTEXENTRY", clobx)
+      printClobSize("ARTICLES", "PDFLINK", clob = false)
+      dbconnx.close()
+      dbShutdown(dbpath)
+    }
+    // clean
+    val pdir = new File(AppStorage.config.dbpath)
+    FileHelper.deleteAll(pdir) // TODO remove later...
+    //    val w = new java.io.PrintWriter(new java.io.OutputStreamWriter(System.out))
+    //    java.sql.DriverManager.setLogWriter(w)
+    debug("checking old db...")
+    dbStats(AppStorage.config.olddbpath, clobx = true)
+    debug("importing db...")
+    java.sql.DriverManager.getConnection(s"jdbc:derby:${AppStorage.config.dbpath};createFrom=${AppStorage.config.olddbpath}")
+    dbShutdown(AppStorage.config.dbpath)
+    debug("upgrading db...")
+    java.sql.DriverManager.getConnection(s"jdbc:derby:${AppStorage.config.dbpath};upgrade=true")
+    dbShutdown(AppStorage.config.dbpath)
+    debug("modify db...")
+    val dbc = java.sql.DriverManager.getConnection(s"jdbc:derby:${AppStorage.config.dbpath}")
+    val s = dbc.createStatement()
+    def modCol(table: String, col: String, default: String = null) = {
+      if (default != null) {
+        s.execute(s"update $table set $col=$default WHERE $col IS NULL")
+        s.execute(s"alter table $table alter column $col default $default")
+      }
+      s.execute(s"alter table $table alter column $col not null")
+    }
+    for (c <- List("TAGS", "KEYWORDS", "RESEARCHGROUP", "CITING", "CITED", "RATING", "PARENT", "PARENT_ID", "AID"))
+      s.execute("alter table ARTICLES drop column " + c)
+
+    s.execute("drop table ARTICLE2ARTICLE")
+
+    modCol("SETTING", "NAME", "''")
+    modCol("SETTING", "VALUE", "''")
+
+    s.execute("alter table TOPIC2ARTICLE drop column PREDECESSORINTOPIC")
+    modCol("TOPIC2ARTICLE", "TOPIC")
+    modCol("TOPIC2ARTICLE", "ARTICLE")
+    modCol("TOPIC2ARTICLE", "COLOR", "0")
+    s.execute("alter table TOPIC2ARTICLE drop foreign key FK345DBEB334742380")
+    s.execute("alter table TOPIC2ARTICLE drop foreign key FK345DBEB3187A628E")
+    s.execute("delete from TOPIC2ARTICLE WHERE ID in ( SELECT MIN(ID) FROM TOPIC2ARTICLE GROUP BY ARTICLE, TOPIC HAVING count(*) > 1) ") // remove errornous duplicates
+    s.execute("""ALTER TABLE "APP"."TOPIC2ARTICLE" ADD CONSTRAINT "TOPIC2ARTICLECPK" UNIQUE ("TOPIC", "ARTICLE")""") // from ddl comparison, this is a useful constraint!
+    s.execute("alter table TOPIC2ARTICLE drop column ID") // id then not needed!
+
+    s.execute("alter table TOPICS drop foreign key FKCC42D924F2885EFB")
+    s.execute("alter table TOPICS drop foreign key FKCC42D924CF8E6BFD")
+    s.execute("update TOPICS SET PARENT=0 WHERE PARENT IS NULL")
+    modCol("TOPICS", "PARENT")
+    s.execute("alter table TOPICS add column EXPANDED boolean not null default false")
+    s.execute("alter table TOPICS alter column TITLE set data type VARCHAR(512)")
+    modCol("TOPICS", "TITLE", "''")
+
+    s.execute("alter table ARTICLES alter column AUTHORS set data type VARCHAR(4096)")
+    s.execute("alter table ARTICLES alter column PDFLINK set data type VARCHAR(10000)")
+    s.execute("alter table ARTICLES drop foreign key FKB6C0D23D4421A2B3")
+
+    s.execute("alter table ARTICLES add column REVIEWNEW VARCHAR(10000)")
+    s.execute("update ARTICLES set REVIEWNEW=REVIEW")
+    s.execute("alter table ARTICLES drop column REVIEW")
+    s.execute("rename column ARTICLES.REVIEWNEW to REVIEW")
+
+    s.execute("alter table ARTICLES add column BIBTEXENTRYNEW VARCHAR(10000)")
+    s.execute("update ARTICLES set BIBTEXENTRYNEW=BIBTEXENTRY")
+    s.execute("alter table ARTICLES drop column BIBTEXENTRY")
+    s.execute("rename column ARTICLES.BIBTEXENTRYNEW to BIBTEXENTRY")
+
+    for (cc <- List("BIBTEXENTRY", "REVIEW", "JOURNAL", "BIBTEXID", "LINKURL", "AUTHORS", "ENTRYTYPE", "PDFLINK", "TITLE", "DOI", "PUBDATE"))
+      modCol("ARTICLES", cc, "''")
+
+    dbc.close()
+    dbShutdown(AppStorage.config.dbpath)
+    debug("checking new db...")
+    dbStats(AppStorage.config.dbpath, clobx = false)
+    info("Upgrade4to5 finished!")
+  }
+
+}
