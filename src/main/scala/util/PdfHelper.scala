4d60a71bd414cb669147951a7035e0a0af2961a3
wolfgang <wolfgang@localhost>
2015-06-11 22:40:58 +0200

    dont parse encrypted pdfs

--- src/main/scala/util/PdfHelper.scala
+++ src/main/scala/util/PdfHelper.scala
@@ -19,43 +19,47 @@ object PdfHelper extends Logging {
   def getDOI(file: File) = {
     var doi = ""
     val pdf = PDDocument.load(file)
-    val info = pdf.getDocumentInformation
-    debug("parse pdf metadata for doi...")
-    if (info.getMetadataKeys.contains("doi")) {
-      doi = info.getCustomMetadataValue("doi")
-    } else if (info.getMetadataKeys.contains("DOI")) {
-      doi = info.getCustomMetadataValue("DOI")
-    } else { // parse for DOI in all keys...
-      for (k <- info.getMetadataKeys) {
-        val v = Option(info.getCustomMetadataValue(k)).getOrElse("").toUpperCase
-        debug(s"pdf metadata [$k]: " + v)
-        v match {
-          case doire.unanchored(sd) => debug("   match!!!"); doi = sd
-          case _ =>
+    if (!pdf.isEncrypted) {
+      val info = pdf.getDocumentInformation
+      debug("parse pdf metadata for doi...")
+      if (info.getMetadataKeys.contains("doi")) {
+        doi = info.getCustomMetadataValue("doi")
+      } else if (info.getMetadataKeys.contains("DOI")) {
+        doi = info.getCustomMetadataValue("DOI")
+      } else {
+        // parse for DOI in all keys...
+        for (k <- info.getMetadataKeys) {
+          val v = Option(info.getCustomMetadataValue(k)).getOrElse("").toUpperCase
+          debug(s"pdf metadata [$k]: " + v)
+          v match {
+            case doire.unanchored(sd) => debug("   match!!!"); doi = sd
+            case _ =>
+          }
         }
       }
-    }
-    if (doi == "") { // parse for doi in first pdf page (not checking hyperlinks as it may be a reference link!)
-      debug("still no doi, get first pdf page...")
-      val doc = pdf.getDocument
-      val pdoc = new PDDocument(doc)
-      val pstrip = new PDFTextStripper()
-      pstrip.setStartPage(1)
-      pstrip.setEndPage(1)
-      val text = pstrip.getText(pdoc)
-      debug("search first page for doi link...")
-      // debug("first page:\n" + text)
-      doi = doire.findFirstIn(text).getOrElse("")
       if (doi == "") {
-        val text2 = text.replaceAll("""[\r\n]""", "")
-        debug("found no doi, check for vertical arxiv id...")
-        //debug("first page without line ends:\n" + text2)
-        val re2 = """.*arXiv:(\d+\.\d+)(?:v\d+)*\s.*""".r
-        text2 match {
-          case re2(aaa) =>
-            debug("  found arxiv id: " + aaa)
-            doi = "arxiv:" + aaa
-          case _ => debug("could not find doi on first pdf page!")
+        // parse for doi in first pdf page (not checking hyperlinks as it may be a reference link!)
+        debug("still no doi, get first pdf page...")
+        val doc = pdf.getDocument
+        val pdoc = new PDDocument(doc)
+        val pstrip = new PDFTextStripper()
+        pstrip.setStartPage(1)
+        pstrip.setEndPage(1)
+        val text = pstrip.getText(pdoc)
+        debug("search first page for doi link...")
+        // debug("first page:\n" + text)
+        doi = doire.findFirstIn(text).getOrElse("")
+        if (doi == "") {
+          val text2 = text.replaceAll( """[\r\n]""", "")
+          debug("found no doi, check for vertical arxiv id...")
+          //debug("first page without line ends:\n" + text2)
+          val re2 = """.*arXiv:(\d+\.\d+)(?:v\d+)*\s.*""".r
+          text2 match {
+            case re2(aaa) =>
+              debug("  found arxiv id: " + aaa)
+              doi = "arxiv:" + aaa
+            case _ => debug("could not find doi on first pdf page!")
+          }
         }
       }
     }
